a674201a0e876d907e70e950f27692c373fdb645
CaptainLiu1206 <user.name>
2019-01-15 20:31:22 +0800

    fist add

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">{{pagename}}</div>    
+</template>
+<script>
+export default {
+    name: 'Home',
+    asyncData ({store, route}) {
+    },
+    title () {
+        return 'home1'
+    },
+    data () {
+        return {
+            pagename: 'home page'
+        }
+    }
+}
+</script>
+<style>
+
+</style>
